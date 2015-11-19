231a1be8790cb6f3d0b0e883f60a2f436df00f5a
Sergey Zhizhenko <zlobnyiserg@gmail.com>
2015-11-19 15:28:01 +0300

    Актуализировал интерфейс под более свежую версию сообщений

--- HmsSanatoriumBridge/Handlers/PostPaymentHandler.cs
+++ HmsSanatoriumBridge/Handlers/PostPaymentHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Logus.HMS.Messages.Folio;
+using Logus.HMS.Messages.Invoice;
 using NServiceBus;
 using NServiceBus.Logging;
 
@@ -8,7 +9,7 @@ namespace HmsSanatoriumBridge.Handlers
     /// <summary>
     ///     Обработчик сообщений о начислении услуг из МИС Санаториум
     /// </summary>
-    public class PostPaymentHandler : IHandleMessages<PostTransactionOnFolioRequest>
+    public class PostPaymentHandler : IHandleMessages<PostTransactionsRequest>
     {
         protected static readonly ILog Log = LogManager.GetLogger<PostPaymentHandler>();
         private readonly IBus _bus;
@@ -18,19 +19,29 @@ namespace HmsSanatoriumBridge.Handlers
             _bus = bus;
         }
 
-        public void Handle(PostTransactionOnFolioRequest message)
+        public void Handle(PostTransactionsRequest message)
         {
             try
             {
-                Log.InfoFormat(
-                    "Получен запрос на начисление транзакций:\n PropCode:{0}\n FolioNo:{1}\n TransactionCode:{2}\n Name:{3}",
-                    message.PropertyCode, message.FolioGenericNo, message.TransactionCode, message.Name);
+                Log.InfoFormat("Получен запрос на начисление транзакций:\n PropCode:{0}\n FolioNo:{1}", message.PropertyCode, message.FolioGenericNo);
 
-                // TODO Начилить транзакцию из сообщения message на счёт в HMS-системе. Отправить ответ об успешном завершении, перечислив идентификаторы созданных транзакций                
-                _bus.Reply(new PostTransactionResponse(message)
+                // TODO Начилить транзакции из сообщения message на счёт в HMS-системе.
+                // Отправить ответ об успешном завершении, перечислив идентификаторы созданных транзакций, а также баланс счёта после начисления (опционально)
+                _bus.Reply(new PostTransactionsResponse(message)
                 {
                     Succeeded = true,
-                    Ids = new string[] {"1", "2", "3"}
+                    PostedTransactions = new[]
+                    {
+                        new PostTransactionResponse
+                        {
+                            FolioBalance = new FolioPocketInfo
+                            {
+                                Amount = 100.0m, // баланс счёта после начисления
+                                CurrencyCode = "RUB"
+                            },
+                            Ids = new [] {"1", "2", "3"}
+                        }
+                    }                    
                 });
             }
             catch (ApplicationException ex)
@@ -38,7 +49,7 @@ namespace HmsSanatoriumBridge.Handlers
                 Log.Error("Ошибка при обработке запроса на начисление", ex);
                 // Следует отлавливать только высокоуровневые исключения уровня приложения (когда данная услуга не может быть начислена вследствие некорректности параметров сообщения)
                 // В случаях с инфраструктурными проблемами (недоступен сервер, файл и т.п.) стоит выбрасывать исключение наружу, чтобы шина позаботилась о повторной доставке этого сообщения
-                _bus.Reply(new PostTransactionResponse(message)
+                _bus.Reply(new PostTransactionsResponse(message)
                 {
                     Succeeded = false,
                     ErrorMessage = ex.Message
